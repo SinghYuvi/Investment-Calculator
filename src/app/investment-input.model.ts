ae8eaeb81c05e02dda1b2aacb1bcfcaeb0be651e
unknown <singhyuvraj1995@yahoo.com0>
2024-11-11 17:23:21 +0530

    Final push

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,15 @@
+export interface InvestmentInput {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }
+
+//   Alternate Option by using   
+//   export type InvestmentInput = {
+//     initialInvestment: number;
+//     annualInvestment: number;
+//     expectedReturn: number;
+//     duration: number;
+//   }
+
